d0ed341501f431beab6cfdff342a0ea27e4120c8
Priyanka <155229913+Vivid26@users.noreply.github.com>
2024-08-25 17:42:08 +0530

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8wOg_G032D8YR0rB9mFIgnE66yvSEgtw",
-  authDomain: "coding-ninjas-apps.firebaseapp.com",
-  projectId: "coding-ninjas-apps",
-  storageBucket: "coding-ninjas-apps.appspot.com",
-  messagingSenderId: "162277395932",
-  appId: "1:162277395932:web:675bac2c60d7cd7960d319"
+  // Firebase Configurations created by you for this project.
 };
 
 const app = initializeApp(firebaseConfig);
